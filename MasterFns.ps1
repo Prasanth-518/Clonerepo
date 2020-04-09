8046aa088e59cc053d608ada799ec211b9d921fc
Prasanth <prasanthvg3@gmail.com>
2020-04-09 17:27:32 +0530

    Master Push

---
+++ MasterFns.ps1
@@ -0,0 +1,42 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$operation
+)
+
+#pushing data into remote repo
+function push_into_repo(){
+    git add .
+    echo "Enter commit msg"
+    $msg=read-host
+    git commit -m $msg
+    git push origin master
+
+}
+
+#merging data from other branches to master branch
+function merge_repos(){
+    #get list of all branches
+    $current_dir=get-location
+    $list_branch=ls "$current_dir\.git\refs\remotes\origin"
+    
+    #filter the list
+    $merge_list="master"
+    foreach($branch in $list_branch){
+        if($branch.name -notlike "HEAD" -and $branch.name -notlike "master"){
+            $merge_list="{0} origin/{1}"-f $merge_list,$branch.name
+        }
+    }
+    
+    #perform merge
+    git merge $merge_list
+    git push origin master
+}
+
+switch($operation){
+    "push"{
+        push_into_repo
+    }
+    "merge"{
+        merge_repos
+    }
+}
